b5d78aec4fafdb1e2ac4c43c239e303e0051c614
Rup Zalavadia <rupzalavadia72@gmail.com>
2020-12-14 15:50:06 +0000

    Final Update

---
+++ ReviewRestAPI/src/main/java/com/tcs/reviewrestapi/exception/ExpiryDateException.java
@@ -0,0 +1,9 @@
+package com.tcs.reviewrestapi.exception;
+
+public class ExpiryDateException extends Exception {
+
+	public ExpiryDateException(String msg) {
+		// TODO Auto-generated constructor stub
+		super(msg);
+	}
+}
